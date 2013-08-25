dbe08b0d52d641902a28baf02d3d59926f51f0d8
Ckrag <maxkrag@gmail.com>
2013-08-26 01:16:52 +0200

    v1.0

--- XlsParser/Form1.Designer.cs
+++ XlsParser/Form1.Designer.cs
@@ -28,12 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.parse_xls_btn = new System.Windows.Forms.Button();
+            this.openFileDialogXls = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // parse_xls_btn
+            // 
+            this.parse_xls_btn.Location = new System.Drawing.Point(12, 12);
+            this.parse_xls_btn.Name = "parse_xls_btn";
+            this.parse_xls_btn.Size = new System.Drawing.Size(75, 23);
+            this.parse_xls_btn.TabIndex = 0;
+            this.parse_xls_btn.Text = "Parse Xls";
+            this.parse_xls_btn.UseVisualStyleBackColor = true;
+            this.parse_xls_btn.Click += new System.EventHandler(this.parse_xls_btn_Click);
+            // 
+            // openFileDialogXls
+            // 
+            this.openFileDialogXls.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.parse_xls_btn);
+            this.Name = "Form1";
+            this.Text = "XlsParser";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button parse_xls_btn;
+        private System.Windows.Forms.OpenFileDialog openFileDialogXls;
     }
 }
 
--- XlsParser/Form1.cs
+++ XlsParser/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LinqToExcel;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 
 namespace XlsParser
 {
@@ -15,6 +19,58 @@ namespace XlsParser
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //quick program to parse some excel data to pdf
+
+        private void parse_xls_btn_Click(object sender, EventArgs e)
+        {
+            this.openFileDialogXls.FileName = "*.xls";
+            if (this.openFileDialogXls.ShowDialog() == DialogResult.OK)
+            {
+                var excel = new ExcelQueryFactory(openFileDialogXls.FileName);
+                var people = from c in excel.Worksheet("k")
+                             //where c["Person"] == "Anders Krog"
+                             select c;
+
+
+                //create PDF
+                Document doc = new Document(iTextSharp.text.PageSize.A4, 58, 58, 108, 10);
+
+                var output = new FileStream("brevhoved pdf.pdf", FileMode.Create);
+                var writer = PdfWriter.GetInstance(doc, output);
+
+                doc.Open();
+
+                int page_counter = 0;
+
+                foreach (var row in people)
+                {
+                    if (row["Person"] != "" && row["Postnr og By"] != "" && row["Adresse"] != "")
+                    {
+                        if (page_counter > 0)
+                        {
+                            doc.NewPage();
+                        }
+                        string name = row["Person"];
+                        string city = row["Postnr og By"];
+                        string address = row["Adresse"];
+
+                        doc.Add(new Phrase(name));
+                        doc.Add(new Phrase(Environment.NewLine));
+                        doc.Add(new Phrase(address));
+                        doc.Add(new Phrase(Environment.NewLine));
+                        doc.Add(new Phrase(city));
+
+                        page_counter++;
+
+
+                    }
+                }
+                doc.Close();
+                System.Diagnostics.Process.Start("brevhoved pdf.pdf");
+                Application.Exit();
+            }
         }
     }
 }
